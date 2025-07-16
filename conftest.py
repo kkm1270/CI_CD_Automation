7175424d358a826c148351abab03e60b0d8d4a7b
KKMDB17 <officepk109@gmail.com>
2025-07-17 00:46:15 +0500

    test

--- conftest.py
+++ conftest.py
@@ -1,11 +1,61 @@
 import pytest
-import time
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+import tempfile
 
 @pytest.fixture(scope='session')
 def driver():
-    driver=webdriver.Edge()
-    driver.maximize_window()
+    options = Options()
+    # Create a temporary directory for Edge user data
+    user_data_dir = tempfile.mkdtemp()
+    options.add_argument(f'--user-data-dir={user_data_dir}')
+    # Optional: run in headless if in CI
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import pytest
+# import time
+# from selenium import webdriver
+
+
+# @pytest.fixture(scope='session')
+# def driver():
+#     driver=webdriver.Edge()
+#     driver.maximize_window()
+#     yield driver
+#     driver.quit()
